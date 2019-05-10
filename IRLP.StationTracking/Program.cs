1a2ad3dce572e8aefe6a2fb62a3672b31b84f409
RussellThomas <RussellThomas@CYBERPOWERPC>
2019-05-10 11:51:33 -0500

    updated namespace.

--- IRLP.StationTracking/Program.cs
+++ IRLP.StationTracking/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IRLP.StationTracking
+namespace KV4S.AmateurRadio.IRLP.StationTracking
 {
     class Program
     {
